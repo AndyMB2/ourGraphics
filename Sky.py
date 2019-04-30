c9812deca1b010ff924252ac124df82836398009
csPython <cspython@AD25TF04NGF1J-33.local>
2019-04-30 11:40:58 -0400

    Started cloud function

--- Sky.py
+++ Sky.py
@@ -2,12 +2,20 @@
 from random import randint
 from graphics import *
 
-def draw_circle(cX, cY, radius, cColor1, cColor2):
+def draw_circle(cX, cY, radius, cColor):
     circle = Circle(Point(cX, cY), radius)
-    circle.setFill(cColor1)
-    circle.setOutline(cColor2)
+    circle.setFill(cColor)
+    circle.setOutline(cColor)
     circle.draw(skyWin)
-                
+
+def draw_cloud(maxX, minY, maxY, clRad, clColor): 
+    draw_circle(randint(0, maxX), randint(minY, maxY),clRad , clColor)
+
+##    draw_circle(skySz,skySz-50, 20, "white")
+##    draw_circle(skySz,skySz-50, 20, "white")
+##    draw_circle(skySz/2,skySz-50, 20, "white")
+##    draw_circle(skySz/2,skySz-50, 20, "white")               
+
 skySz = 800
 
 skyWin = GraphWin("SkyScraperScenery", skySz, skySz)
@@ -15,7 +23,7 @@ skyWin.setCoords(0, 0, skySz, skySz)
 
 bgR=Rectangle(Point( 0,0 ), Point(skySz, skySz))
 bgR.setFill("skyblue")
-bgR.draw(skyWin)  
+bgR.draw(skyWin)
 
-draw_circle(skySz/2,skySz-50, 20, "white", "white")
 
+draw_cloud(skySz, 600, 800, 20, "white")
