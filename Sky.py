f7d9a79a209882d565a77f1635d08de1d1c28bec
csPython <cspython@AD25TF04NGF1J-178.local>
2019-04-30 11:46:31 -0400

    added other windows and add door

--- Sky.py
+++ Sky.py
@@ -2,10 +2,10 @@
 
 from graphics import *
 
-def draw_block():
-    sC = Rectangle(Point(250, 0), Point(550, 700))
-    sC.setFill("grey") 
-    sC.draw(skyWin)
+def draw_rect(rX, rY, rW, rH, rCol, rWin):
+    sC = Rectangle(Point(rX, rY), Point(rX + rW, rY + rH))
+    sC.setFill(rCol)
+    sC.draw(rWin)
 
 skyX = 800
 skyY = 800
@@ -13,8 +13,17 @@ skyY = 800
 skyWin = GraphWin("SkyScraperScenery", skyX, skyY)
 skyWin.setCoords(0, 0, skyX, skyY)
 
-bgR=Rectangle(Point( 0,0 ), Point(skyX, skyY))
-bgR.setFill("skyblue")
-bgR.draw(skyWin) 
+draw_rect(0, 0, skyX, skyY, "sky blue", skyWin) #Background
 
-draw_towers()
+gX = 258
+gH = 3.2
+gW = 7
+gCol = "white" 
+
+draw_rect(250, 0, skyX / 3, 700, "grey", skyWin)#Tower
+draw_rect(258, 550, skyX / gH, skyY / gW, gCol, skyWin)#window
+draw_rect(258, 420, skyX/ gH, skyY/ gW, gCol, skyWin)
+draw_rect(258, 290, skyX/ gH, skyY/ gW, gCol, skyWin)
+draw_rect(258, 160, skyX/ gH, skyY/ gW, gCol, skyWin)
+draw_rect(315, 0, skyX/ 6, skyY/ 5.3, "dark grey", skyWin)#door 
+draw_rect(410, 50, skyX/ 40, skyY/ 40, "black", skyWin)#door_nob
