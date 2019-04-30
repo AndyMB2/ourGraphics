9e2d295516e64ad2ac1596b11766851cd0bd474d
csPython <cspython@AD25TF04NGF1J-178.local>
2019-04-30 13:08:02 -0400

    added finally windows and door

--- Sky.py
+++ Sky.py
@@ -1,4 +1,4 @@
-#ourGraphics.py by Andy B & Fernando A
+ #ourGraphics.py by Andy B & Fernando A
 
 from graphics import *
 
@@ -21,9 +21,9 @@ gW = 7
 gCol = "white" 
 
 draw_rect(250, 0, skyX / 3, 700, "grey", skyWin)#Tower
-draw_rect(258, 550, skyX / gH, skyY / gW, gCol, skyWin)#window
-draw_rect(258, 420, skyX/ gH, skyY/ gW, gCol, skyWin)
-draw_rect(258, 290, skyX/ gH, skyY/ gW, gCol, skyWin)
-draw_rect(258, 160, skyX/ gH, skyY/ gW, gCol, skyWin)
+draw_rect(gX, 550, skyX / gH, skyY / gW, gCol, skyWin)#window
+draw_rect(gX, 420, skyX/ gH, skyY/ gW, gCol, skyWin)
+draw_rect(gX, 290, skyX/ gH, skyY/ gW, gCol, skyWin)
+draw_rect(gX, 160, skyX/ gH, skyY/ gW, gCol, skyWin)
 draw_rect(315, 0, skyX/ 6, skyY/ 5.3, "dark grey", skyWin)#door 
 draw_rect(410, 50, skyX/ 40, skyY/ 40, "black", skyWin)#door_nob
